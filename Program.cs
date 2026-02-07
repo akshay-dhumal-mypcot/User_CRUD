5c65823eb30d25b64c17540f9c24ec1ed956145c
Akshay Dhumal <akshay.d@mypcot.com>
2026-02-07 17:44:37 +0530

    Update GitHub Actions workflow for UserCRUD deployment

--- Program.cs
+++ Program.cs
@@ -14,11 +14,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "User CRUD API V1");
+    c.RoutePrefix = "swagger"; // optional, default is "swagger"
+});
+
 
 app.UseHttpsRedirection();
 
